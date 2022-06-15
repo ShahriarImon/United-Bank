c1ac7312dda2f6ecc89b279740edc5e29293410a
ShahriarImon <imon.shahriar012@gmail.com>
2022-06-15 16:20:19 +0600

    null input, negetive amount fixes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 document.getElementById('login').addEventListener('click', function (event) {
     document.getElementById('login-area').style.display = 'none';
     document.getElementById('transaction-area').style.display = 'block';
-
+    
 })
-// ............This function is used only for (-1 multiplication for balance to add negetive value since withdraw)
+
+// ............This function is used only for (-1 multiplication for balance to add negative value since withdraw)
 function inputStringToValue(inputId) {
     depositAmount = document.getElementById(inputId).value;
+    console.log(depositAmount);
     return parseFloat(depositAmount);
 }
 
@@ -16,11 +18,30 @@ function transaction(currentAmountId, inputedAmount) {
 
 document.getElementById('depositBtn').addEventListener('click', function () {
     depositNumber = inputStringToValue('depositAmount');
-    transaction('currentDeposit', depositNumber);
-    transaction('currentBalance', depositNumber);
+    if (document.getElementById('depositAmount').value == '') {
+        alert("Hey, Don't Click Deposit Button without inserting Amount")
+    } else {
+
+
+        if (depositNumber < 0) {
+            alert("Hey, Don't insert negative amount")
+        } else {
+            transaction('currentDeposit', depositNumber);
+            transaction('currentBalance', depositNumber);
+        }
+    }
 })
+
 document.getElementById('withdrawBtn').addEventListener('click', function () {
     withdrawNumber = inputStringToValue('withdrawAmount');
-    transaction('currentWithdraw', withdrawNumber);
-    transaction('currentBalance', -1*withdrawNumber);
+    if (document.getElementById('depositAmount').value == '') {
+        alert("Hey, Don't Click Withdraw Button without inserting Amount")
+    } else {
+        if (withdrawNumber < 0) {
+            alert("Hey, Don't insert negative amount")
+        } else {
+            transaction('currentWithdraw', withdrawNumber);
+            transaction('currentBalance', -1 * withdrawNumber);
+        }
+    }
 })
